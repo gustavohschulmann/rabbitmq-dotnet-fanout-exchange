0cd58b038bab4372b4bd8f0aeccb5f8a220151a1
gustavoschulmann <gustavohschulmann@gmail.com>
2022-08-10 23:49:23 -0300

    first commit

---
+++ FanoutConsumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace FanoutConsumer
+{
+    class Program
+    {
+        static IConnection conn;
+        static IModel channel;
+        
+        static void Main(string[] args)
+        {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            //Create connection and channel
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+
+            //Create a consumer object
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += ConsumerReceived;
+            
+            //Subscribe to the queue
+            var consumerTag = channel.BasicConsume("my.queue1", true, consumer);
+            
+            Console.WriteLine("Waiting for the messages. Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void ConsumerReceived(object sender, BasicDeliverEventArgs e)
+        {
+            string message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine("Message: " + message);
+            
+            //Acknowledge to rabbitMQ we processed the message with this DeliveryTag and it's ok to remove it from the queue
+            channel.BasicAck(e.DeliveryTag, false);
+            
+            //Or if you want to deny the receive of the message, due to an internal error or something
+            //channel.BasicNack(e.DeliveryTag, false, true); //This true tell to put the message again on queue
+        }
+    }
+}
---
+++ FanoutPublisher/Program.cs
@@ -0,0 +1,57 @@
+﻿// •	Producer: Application that sends the messages.
+// •	Consumer: Application that receives the messages.
+// •	Queue: Buffer that stores messages.
+// •	Message: Information that is sent from the producer to a consumer through RabbitMQ.
+// •	Connection: A TCP connection between your application and the RabbitMQ broker.
+// •	Channel: A virtual connection inside a connection. When publishing or consuming messages from a queue - it's all done over a channel.
+// •	Exchange: Receives messages from producers and pushes them to queues depending on rules defined by the exchange type. To receive messages, a queue needs to be bound to at least one exchange.
+
+using System.Text;
+using RabbitMQ.Client;
+
+namespace FanoutPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnection conn;
+            IModel channel;
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            //Create connection and channel
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+
+            //Create Exchange and Queues
+            channel.ExchangeDeclare("ex.fanout", "fanout", true, false, null);
+            channel.QueueDeclare("my.queue1", true, false, false, null);
+            channel.QueueDeclare("my.queue2", true, false, false, null);
+            
+            channel.QueueBind("my.queue1", "ex.fanout", "");
+            channel.QueueBind("my.queue2", "ex.fanout", "");
+            
+            //Send encoded message
+            channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message 1"));
+            channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message Number 2"));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            
+            //Delete queues and exchange
+            channel.QueueDelete("my.queue1");
+            channel.QueueDelete("my.queue2");
+            channel.ExchangeDelete("ex.fanout");
+            
+            //Close connection and channel
+            channel.Close();
+            conn.Close();
+        }
+    }
+}
